45597438287b87fc81db019e489a0bc001346876
Aashique Kumar Singh <singhashique15@gmail.com>
2025-09-06 13:20:57 +0530

    mobile responsive

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,11 @@
 const faqItems = document.querySelectorAll(".faq-item");
 
-faqItems.forEach(item => {
+faqItems.forEach((item) => {
   const btn = item.querySelector(".faq-question");
 
   btn.addEventListener("click", () => {
     // close other items
-    faqItems.forEach(i => {
+    faqItems.forEach((i) => {
       if (i !== item) {
         i.classList.remove("active");
         i.querySelector(".icon").textContent = "+";
@@ -18,4 +18,16 @@ faqItems.forEach(item => {
     const icon = item.querySelector(".icon");
     icon.textContent = item.classList.contains("active") ? "–" : "+";
   });
+});
+
+const hamburger = document.querySelector(".humburger");
+const mobileNav = document.querySelector(".mobile_nav");
+const closeBtn = document.querySelector(".close_btn");
+
+hamburger.addEventListener("click", () => {
+  mobileNav.classList.add("active"); // open from left
+});
+
+closeBtn.addEventListener("click", () => {
+  mobileNav.classList.remove("active"); // close to left
 });
